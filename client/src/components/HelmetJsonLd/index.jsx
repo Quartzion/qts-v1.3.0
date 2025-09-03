e733e56701df2d3cff0f5426a417479b1d3cb9f9
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-09-03 16:48:28 -0400

    Improved schema for services to products

--- client/src/components/HelmetJsonLd/index.jsx
+++ client/src/components/HelmetJsonLd/index.jsx
@@ -4,6 +4,7 @@ import slugify from 'slugify';
 import blogs from '../../utils/blogData';
 import services from '../../utils/servicesData';
 const baseUrl = import.meta.env.VITE_BASE_URL;
+const qtsPhoneNumber = import.meta.env.VITE_QTS_PHONE;
 
 export default function HelmetJsonLd() {
 
@@ -14,10 +15,49 @@ export default function HelmetJsonLd() {
                 "@type": "WebSite",
                 "name": "Quartzion-dev.com",
                 "url": baseUrl,
+                "publisher": {
+                    "@id": "https://yourdomain.com/#organization"
+                },
                 "potentialAction": {
                     "@type": "SearchAction",
                     "target": `${baseUrl}/?q={search_term_string}`,
                     "query-input": "required name=search_term_string"
+                }
+            },
+            {
+                "@type": "Organization",
+                "@id": `${baseUrl}/#organization`,
+                "name": "Quartzion Technology Solutions Corp.",
+                "url": `${baseUrl}`,
+                "logo": {
+                    "@type": "ImageObject",
+                    "url": `${baseUrl}/qts-icon-2.webp`
+                },
+                "sameAs": [
+                    "https://www.linkedin.com/company/quartzion-technology-solutions-corp",
+                    "https://twitter.com/QuartzionTech"
+                ]
+            },
+            {
+                "@type": "LocalBusiness",
+                "@id": `${baseUrl}/#localbusiness`,
+                "name": "Quartzion Technology Solutions Corp.",
+                "image": {
+                    "@type": "ImageObject",
+                    "url": `${baseUrl}/qts-icon-2.webp`
+                },
+                "address": {
+                    "@type": "PostalAddress",
+                    "streetAddress": "690 Main St 253",
+                    "addressLocality": "Safety Harbor",
+                    "addressRegion": "FL",
+                    "postalCode": "34695",
+                    "addressCountry": "US"
+                },
+                "telephone": `${qtsPhoneNumber}`,
+                "url": `${baseUrl}`,
+                "parentOrganization": {
+                    "@id": `${baseUrl}/#organization`
                 }
             },
             ...blogs.map(blog => ({
@@ -54,24 +94,21 @@ export default function HelmetJsonLd() {
             })),
 
             ...services.map(service => ({
-                "@type": "Service",
+                "@type": "Product",
+                "@id": `${baseUrl}/services?slug=${slugify(service.title, { lower: true, strict: true })}`,
                 "name": service.title,
                 "description": service.description || service.content.slice(0, 160),
-                "provider": {
-                    "@type": "Organization",
-                    "name": "Quartzion Technology Solutions Corp.",
-                    "logo": {
-                        "@type": "ImageObject",
-                        "url": `${baseUrl}/qts-icon-2.webp`
-                    }
+                "brand": {
+                    "@id": `${baseUrl}/#organization`
                 },
-                "areaServed": "United States",
-                "serviceType": "Technology",
-                "keywords": "technology, nonprofit, innovation, Quartzion, community, development, 501c3",
-                "url": `${baseUrl}/services?slug=${slugify(service.title, { lower: true, strict: true })}`,
-                "image": `${baseUrl}${service.img.replace(/^\./, '')}`,
+                "offers": {
+                    "@type": "Offer",
+                    "url": `${baseUrl}/services?slug=${slugify(service.title, { lower: true, strict: true })}`,
+                    "priceCurrency": "USD",
+                    "price": "0.00",
+                    "availability": "https://schema.org/InStock"
+                }
             }))
-
         ],
     };
 
